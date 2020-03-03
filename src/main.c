37d0612d299af806d8ed6a51d0cd2f6bf3b2fe7b
jenra <jenra.uwu@gmail.com>
2020-03-03 08:56:05 -0500

    Initial upload

---
+++ src/main.c
@@ -0,0 +1,17 @@
+//
+// Curly
+// main.c: Runs the command line interface.
+//
+// jenra
+// March 3 2020
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+	puts("Hello world");
+	return 0;
+}
