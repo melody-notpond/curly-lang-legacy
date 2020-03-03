0d80bd8e8bfc0f3bf71f89e82b88b3c349009309
jenra <jenra.uwu@gmail.com>
2020-03-03 18:15:15 -0500

    Added character and symbol parsers

--- src/parser.c
+++ src/parser.c
@@ -14,9 +14,11 @@ comb_t* create_lang_parser()
 {
 	comb_t* integer = c_name("int", c_regex("-?[0-9]+"));
 	comb_t* floating = c_name("float", c_regex("-?[0-9]+(\\.[0-9]+([eE][+-]?[0-9]+)?|(\\.[0-9]+)?[eE][+-]?[0-9]+)"));
-	comb_t* value = c_or(floating, integer, NULL);
+	comb_t* character = c_name("char", c_regex("'([^'\\\\]|\\\\(x[0-9a-fA-F]{2}|[^x]))'"));
+	comb_t* symbol = c_name("symbol", c_regex("[_a-zA-Z][_a-zA-Z0-9]*'*"));
+	comb_t* value = c_or(floating, integer, character, symbol, NULL);
 
-	comb_t* parser = c_eof(value);
+	comb_t* parser = c_eof(c_zmore(value));
 	parser->ignore_whitespace = true;
 	return parser;
 }
