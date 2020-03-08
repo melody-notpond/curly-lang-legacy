99c9fe243585e8b738d3084fbab835ab05fe3b7f
jenra <jenra.uwu@gmail.com>
2020-03-07 19:50:41 -0500

    Lots of refactoring

--- src/parser.c
+++ src/parser.c
@@ -13,24 +13,27 @@
 comb_t* create_lang_parser()
 {
 	comb_t* expr = init_combinator();
+	comb_t* assign = init_combinator();
 	comb_t* comprehension = init_combinator();
-
-	comb_t* if_state = c_name("if", c_seq(
-		c_ignore(c_str("if")), c_name("cond", expr),
-		c_ignore(c_str("then")), c_name("then", expr),
-		c_optional(c_seq(
-			c_ignore(c_str("else")), c_name("else", expr)
-		))
-	));
 
 	comb_t* primatives = c_name("prim", c_regex("true|false|nil|pass|stop"));
 	comb_t* integer = c_name("int", c_regex("-?[0-9]+"));
 	comb_t* decimal = c_name("float", c_regex("-?[0-9]+(\\.[0-9]+([eE][+-]?[0-9]+)?|(\\.[0-9]+)?[eE][+-]?[0-9]+)"));
 	comb_t* character = c_name("char", c_regex("'([^'\\\\]|\\\\(x[0-9a-fA-F]{2}|[^x]))'"));
 	comb_t* symbol = c_name("symbol", c_regex("[_a-zA-Z][_a-zA-Z0-9]*'*"));
+
+	comb_t* if_state = c_name("if", c_seq(
+		c_ignore(c_str("if")), expr,
+		c_ignore(c_str("then")), assign,
+		c_optional(c_seq(
+			c_ignore(c_str("else")), assign
+		))
+	));
+
 	comb_t* value = c_or(
+		if_state,
 		primatives, decimal, integer, character, symbol,
-		comprehension, if_state,
+		comprehension,
 		c_seq(c_ignore(c_char('(')), expr, c_ignore(c_char(')'))),
 		NULL
 	);
@@ -41,7 +44,7 @@ comb_t* create_lang_parser()
 
 	comb_t* mult = c_name("infix", c_seq(
 		value, c_zmore(
-			c_seq(c_name("op", c_regex("[/*]")), prefix)
+			c_seq(c_name("op", c_regex("[/*%]")), prefix)
 		)
 	));
 
@@ -69,36 +72,46 @@ comb_t* create_lang_parser()
 		)
 	));
 
-	comb_t* assign = c_name("assign", c_seq(
-		symbol, c_ignore(c_char('=')), or
-	));
-
-	comb_t* with = c_name("with", c_seq(
-		c_ignore(c_str("with")), c_omore(c_seq(assign, c_char(',')))
-	));
+	comb_t* with_vars = c_seq(
+		c_ignore(c_str("with")), c_omore(c_seq(assign, c_ignore(c_char(','))))
+	);
 
 	comb_t* for_loop = c_name("for", c_seq(
-		c_ignore(c_str("for")), c_optional(c_name("quantif", c_regex("all|some"))),
-			symbol, c_ignore(c_str("in")), c_name("iter", expr),
-		c_name("body", expr)
+		c_ignore(c_str("for")), c_optional(c_name("quantifier", c_regex("all|some"))),
+			symbol, c_ignore(c_str("in")), expr,
+		assign
 	));
 
-	comb_t* such_that = c_name("st", c_seq(
-		symbol, c_ignore(c_str("in")), c_name("iter", expr),
+	comb_t* such_that = c_name("such that", c_seq(
+		symbol, c_ignore(c_str("in")), expr,
 		c_ignore(c_regex("such[:blank:]+that")),
-		c_name("cond", expr)
+		expr
 	));
 
-	comb_t* comp_body = c_or(c_seq(c_optional(with), for_loop), such_that);
-	c_set(comprehension, c_name("comp", c_or(
+	comb_t* comp_body = c_or(
+		c_name("with",
+			c_seq(c_optional(with_vars), for_loop)
+		), such_that
+	);
+
+	c_set(comprehension, c_name("comprehension", c_or(
 		c_seq(c_char('['), comp_body, c_char(']')),
 		c_seq(c_char('('), comp_body, c_char(')')),
 		c_seq(c_char('{'), comp_body, c_char('}'))
 	)));
 
-	c_set(expr, c_or(assign, add));
+	c_set(assign, c_name("assign", c_seq(
+		c_optional(c_seq(
+			symbol, c_ignore(c_char('='))
+		)), c_name("with", c_seq(
+			c_optional(with_vars),
+			expr
+		))
+	)));
 
-	comb_t* parser = c_eof(c_name("root", c_zmore(expr)));
+	c_set(expr, or);
+
+	comb_t* parser = c_eof(c_name("root", assign));
 	parser->ignore_whitespace = true;
 	return parser;
 }
