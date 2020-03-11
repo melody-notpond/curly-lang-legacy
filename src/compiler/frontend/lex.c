fcaff3889c7802b026dd8878421da7fd9ee51846
jenra <jenra.uwu@gmail.com>
2020-03-11 17:10:20 -0400

    Created decimal lexer

--- src/compiler/frontend/lex.c
+++ src/compiler/frontend/lex.c
@@ -8,6 +8,68 @@
 
 #include "lex.h"
 
+//-?[0-9]+(\.[0-9]+([eE][+-]?[0-9]+)?|(\.[0-9]+)?[eE][+-]?[0-9]+)
+bool create_decimal(lexer_t* lex, lexeme_t* token, bool dot_mode)
+{
+	// Save
+	lexer_t save;
+	lex_save(lex, &save);
+
+	// Following a decimal point
+	if (dot_mode)
+	{
+		// Check that the next character is a digit
+		char c = lex_next_char(lex);
+		if (c < '0' || c > '9')
+			return false;
+		do
+		{
+			// Save and get the next character
+			lex_save(lex, &save);
+			c = lex_next_char(lex);
+
+			// Check if it's scientific notation time
+			if (c == 'e' || c == 'E')
+				dot_mode = false;
+
+		// Do this until the character is not a digit
+		} while ('0' <= c && c <= '9');
+
+		// Revert if not going to check scientific notation and set the type
+		if (dot_mode)
+			lex_revert(lex, &save);
+		token->type = LEX_TYPE_DECIMAL;
+	}
+
+	// Scientific notation
+	if (!dot_mode)
+	{
+		// Optional +/-
+		char c = lex_next_char(lex);
+		if (c == '+' || c == '-')
+			c = lex_next_char(lex);
+
+		// Check if character is a digit
+		if (c < '0' || c > '9')
+		{
+			lex_revert(lex, &save);
+			return token->type == LEX_TYPE_DECIMAL;
+		}
+		do
+		{
+			// Save and get the next character
+			lex_save(lex, &save);
+			c = lex_next_char(lex);
+		// Do this until the character is not a digit
+		} while ('0' <= c && c <= '9');
+
+		// Revert to last character and set the type
+		lex_revert(lex, &save);
+		token->type = LEX_TYPE_DECIMAL;
+	}
+	return true;
+}
+
 // create_int(lexer_t*, lexeme_t*) -> void
 // Creates an integer.
 void create_int(lexer_t* lex, lexeme_t* token)
@@ -26,7 +88,13 @@ void create_int(lexer_t* lex, lexeme_t* token)
 		// Still an integer
 		if ('0' <= c && c <= '9')
 			lex_save(lex, &save);
-		else
+		else if (c == '.' || c == 'e' || c == 'E')
+		{
+			// Check if it's a decimal and backtrack if not
+			if (!create_decimal(lex, token, c == '.') && token->type != LEX_TYPE_INVALID)
+				lex_revert(lex, &save);
+			break;
+		} else
 		{
 			// No longer an integer
 			lex_revert(lex, &save);
