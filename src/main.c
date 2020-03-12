1eab9888a46117acecf832b0ee6bab889bbf0fc5
jenra <jenra.uwu@gmail.com>
2020-03-11 21:54:09 -0400

    Recovered main.c

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "compiler/frontend/lex.h"
+#include "compiler/frontend/parser.h"
 
 int main(int argc, char** argv)
 {
@@ -20,21 +20,12 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	lexer_t lex = lex_str(argv[1], true, curly_lexer_func);
+	parser_t parser = create_lang_parser();
 
-	lexeme_t* token = lex_next_token(&lex);
-	printf("token: %i: \"%s\"\n", token->type, token->string);
-	free(token->string);
+	parse_result_t res = parse_file(parser, argv[1]);
+	print_parse_result(res);
 
-	clean_lex(&lex);
-
-
-	// parser_t parser = create_lang_parser();
-
-	// parse_result_t res = parse_file(parser, argv[1]);
-	// print_parse_result(res);
-
-	// clean_parse_result(&res);
-	// clean_combinator(parser.comb);
+	clean_parse_result(&res);
+	clean_combinator(parser.comb);
 	return 0;
 }
