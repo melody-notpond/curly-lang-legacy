a7f54688acb8d8978706e6b0a6c333ea5fe34ad0
jenra <jenra.uwu@gmail.com>
2020-03-14 18:01:25 -0400

    Implemented stepping (running a single instruction)

--- src/vm/vm.c
+++ src/vm/vm.c
@@ -1,6 +1,6 @@
 //
 // Curly
-// vm.c: Implements the virtual machine
+// vm.c: Implements the virtual machine.
 //
 // jenra
 // March 14 2020
@@ -8,4 +8,29 @@
 
 #include "vm.h"
 
+// init_vm(CurlyVM*) -> void
+// Initialises a vm.
+void init_vm(CurlyVM* vm)
+{
+	vm->chunk = NULL;
+	vm->pc = NULL;
 
+	if (opcode_funcs[0] == NULL)
+		init_opcodes();
+}
+
+// stepi(CurlyVM*) -> void
+// Steps the vm one instruction.
+void stepi(CurlyVM* vm)
+{
+	vm->pc += opcode_funcs[*vm->pc](vm);
+}
+
+// clean_vm(CurlyVM*) -> void
+// Cleans up a vm.
+void clean_vm(CurlyVM* vm)
+{
+	clean_chunk(vm->chunk);
+	vm->chunk = NULL;
+	vm->pc = NULL;
+}
--- src/vm/vm.h
+++ src/vm/vm.h
@@ -10,6 +10,7 @@
 #define vm_h
 
 #include "bytecode.h"
+#include "opcodes.h"
 
 // Represents a virtual machine for curly.
 typedef struct
@@ -20,5 +21,17 @@ typedef struct
 	// The program counter.
 	uint8_t* pc;
 } CurlyVM;
+
+// init_vm(CurlyVM*) -> void
+// Initialises a vm.
+void init_vm(CurlyVM* vm);
+
+// stepi(CurlyVM*) -> void
+// Steps the vm one instruction.
+void stepi(CurlyVM* vm);
+
+// clean_vm(CurlyVM*) -> void
+// Cleans up a vm.
+void clean_vm(CurlyVM* vm);
 
 #endif /* vm_h */
