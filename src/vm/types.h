f9cbc6788b2e4d23160a045b1c7c48339aa6f855
jenra <jenra.uwu@gmail.com>
2020-03-14 20:48:47 -0400

    Added types.h, which defines a number type

---
+++ src/vm/types.h
@@ -0,0 +1,15 @@
+//
+// Curly
+// types.h: Defines various types used by the virtual machine.
+//
+// jenra
+// March 14 2020
+//
+
+#include <inttypes.h>
+
+typedef union
+{
+	int64_t i64;
+	double f64;
+} cnumb_t;
