7d17ec3ae6f4e1e04f0daf29166695e69f2bba74
jenra <jenra.uwu@gmail.com>
2020-03-15 08:39:56 -0400

    Modified test code

--- src/main.c
+++ src/main.c
@@ -19,18 +19,26 @@ int main(int argc, char** argv)
 	chunk_t chunk = init_chunk();
 	chunk_add_i64(&chunk, -5);
 	chunk_add_i64(&chunk, 23);
-	write_chunk(&chunk, OPCODE_MUL_I64_I64);
+	write_chunk(&chunk, OPCODE_SUB_I64_I64);
+	write_chunk(&chunk, OPCODE_PRINT_I64);
+
 	chunk_add_i64(&chunk, 2);
 	chunk_add_f64(&chunk, 2.5);
-	write_chunk(&chunk, OPCODE_MUL_I64_F64);
+	write_chunk(&chunk, OPCODE_SUB_I64_F64);
+	write_chunk(&chunk, OPCODE_PRINT_I64);
+
 	chunk_add_f64(&chunk, 3.14159);
 	chunk_add_i64(&chunk, 2);
-	write_chunk(&chunk, OPCODE_MUL_F64_I64);
+	write_chunk(&chunk, OPCODE_SUB_F64_I64);
+	write_chunk(&chunk, OPCODE_PRINT_F64);
+
 	chunk_add_f64(&chunk, 3.14159);
 	chunk_add_f64(&chunk, 2.71828);
-	write_chunk(&chunk, OPCODE_MUL_F64_F64);
+	write_chunk(&chunk, OPCODE_SUB_F64_F64);
+	write_chunk(&chunk, OPCODE_PRINT_F64);
+
 	write_chunk(&chunk, OPCODE_BREAK);
-	
+
 	disassemble(&chunk, "test.o");
 	puts("\n");
 
