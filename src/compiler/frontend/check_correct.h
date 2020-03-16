e7050de09b9a8d0fcbfa9f034787a072c5198773
jenra <jenra.uwu@gmail.com>
2020-03-15 20:05:21 -0400

    Created check_correct.[hc]

---
+++ src/compiler/frontend/check_correct.h
@@ -0,0 +1,14 @@
+//
+// Curly
+// check_correct.h: Header file for check_correct.c.
+//
+// jenra
+// March 15 2020
+//
+
+#ifndef check_correct_h
+#define check_correct_h
+
+
+
+#endif /* check_correct_h */
