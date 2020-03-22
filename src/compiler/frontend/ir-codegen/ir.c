bd89172a25dc1bae49ee5681b18be852be1a2282
jenra <jenra.uwu@gmail.com>
2020-03-21 21:36:09 -0400

    Added init and clean functions

--- src/compiler/frontend/ir-codegen/ir.c
+++ src/compiler/frontend/ir-codegen/ir.c
@@ -6,6 +6,88 @@
 // March 21 2020
 //
 
+#include <stdlib.h>
+
 #include "ir.h"
 
+// init_ir_arg(void) -> ir_arg_t
+// Initialises an empty ir arg.
+ir_arg_t init_ir_arg()
+{
+	ir_arg_t arg;
+	arg.type = 0;
+	arg.value = NULL;
+	return arg;
+}
 
+// init_ir_line(void) -> ir_line_t
+// Initialises an empty ir line.
+ir_line_t init_ir_line()
+{
+	ir_line_t line;
+	line.op = NULL;
+	line.args[0] = init_ir_arg();
+	line.args[1] = init_ir_arg();
+	return line;
+}
+
+// init_ir_block(void) -> ir_block_t
+// Initialises an empty ir block.
+ir_block_t init_ir_block()
+{
+	ir_block_t block;
+	block.lines = NULL;
+	block.count = 0;
+	block.size = 0;
+	block.jump_type = 0;
+	block.cond_jump = NULL;
+	return block;
+}
+
+// init_ir(void) -> curly_ir_t
+// Initialises an empty ir.
+curly_ir_t init_ir()
+{
+	curly_ir_t ir;
+	ir.blocks = malloc(sizeof(ir_block_t));
+	*ir.blocks = init_ir_block();
+	ir.count = 0;
+	ir.size = 0;
+	return ir;
+}
+
+// clean_ir(curly_ir_t*) -> void
+// Cleans an ir.
+void clean_ir(curly_ir_t* ir)
+{
+	for (int i = 0; i < ir->count; i++)
+	{
+		clean_ir_line(ir->blocks + i);
+	}
+}
+
+// clean_ir_block(ir_block_t*) -> void
+// Cleans an ir block.
+void clean_ir_block(ir_block_t* block)
+{
+	for (int i = 0; i < block->count; i++)
+	{
+		clean_ir_line(block->lines + i);
+	}
+}
+
+// clean_ir_line(ir_line_t*) -> void
+// Cleans an ir line.
+void clean_ir_line(ir_line_t* line)
+{
+	free(line->op);
+	clean_ir_arg(line->args    );
+	clean_ir_arg(line->args + 1);
+}
+
+// clean_ir_arg(ir_arg_t*) -> void
+// Cleans an ir arg.
+void clean_ir_arg(ir_arg_t* arg)
+{
+	free(arg->value);
+}
--- src/compiler/frontend/ir-codegen/ir.h
+++ src/compiler/frontend/ir-codegen/ir.h
@@ -29,7 +29,7 @@ typedef struct
 	char* op;
 
 	// The arguments of the line.
-	ir_arg_t* args;
+	ir_arg_t args[2];
 } ir_line_t;
 
 // Represents a basic block of ir code.
@@ -38,6 +38,12 @@ typedef struct s_ir_block
 {
 	// The lines of code in the basic block.
 	ir_line_t* lines;
+
+	// The number of lines.
+	int count;
+
+	// The amount of memory allocated for the lines.
+	int size;
 
 	// The type of the jump.
 	int8_t jump_type;
@@ -51,6 +57,44 @@ typedef struct
 {
 	// The list of blocks.
 	ir_block_t* blocks;
+
+	// The number of blocks.
+	int count;
+
+	// The amount of memory allocated for the blocks.
+	int size;
 } curly_ir_t;
+
+// init_ir_arg(void) -> ir_arg_t
+// Initialises an empty ir arg.
+ir_arg_t init_ir_arg();
+
+// init_ir_line(void) -> ir_line_t
+// Initialises an empty ir line.
+ir_line_t init_ir_line();
+
+// init_ir_block(void) -> ir_block_t
+// Initialises an empty ir block.
+ir_block_t init_ir_block();
+
+// init_ir(void) -> curly_ir_t
+// Initialises an empty ir.
+curly_ir_t init_ir();
+
+// clean_ir(curly_ir_t*) -> void
+// Cleans an ir.
+void clean_ir(curly_ir_t* ir);
+
+// clean_ir_block(ir_block_t*) -> void
+// Cleans an ir block.
+void clean_ir_block(ir_block_t* block);
+
+// clean_ir_line(ir_line_t*) -> void
+// Cleans an ir line.
+void clean_ir_line(ir_line_t* line);
+
+// clean_ir_arg(ir_arg_t*) -> void
+// Cleans an ir arg.
+void clean_ir_arg(ir_arg_t* arg);
 
 #endif /* curly_ir_h */
