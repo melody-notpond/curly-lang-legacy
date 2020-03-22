527a0a6f4427e69e555bd2896334ae2748c538fe
jenra <jenra.uwu@gmail.com>
2020-03-21 20:56:15 -0400

    Added gencode.[hc]

---
+++ src/compiler/frontend/ir-codegen/gencode.c
@@ -0,0 +1,10 @@
+//
+// Curly
+// gencode.c: Generates ir code from an ast and checks correctness.
+//
+// jenra
+// March 21 2020
+//
+
+#include "gencode.h"
+
---
+++ src/compiler/frontend/ir-codegen/gencode.h
@@ -0,0 +1,16 @@
+//
+// Curly
+// gencode.h: Header file for gencode.c.
+//
+// jenra
+// March 21 2020
+//
+
+#ifndef gencode_h
+#define gencode_h
+
+#include "combinator.h"
+#include "ir.h"
+
+
+#endif /* gencode_h */
