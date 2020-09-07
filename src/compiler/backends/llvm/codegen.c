2d7b5212ea9056fd7c899b6d12699fcc65150ec1
jenra <jenra.uwu@gmail.com>
2020-09-07 11:28:00 -0400

    Added codegen.c and codegen.h

---
+++ src/compiler/backends/llvm/codegen.c
@@ -0,0 +1,11 @@
+//
+// Curly
+// codegen.c: Implements code generation to LLVM IR.
+//
+// Created by jenra.
+// Created on September 7 2020.
+//
+
+#include "codegen.h"
+
+
---
+++ src/compiler/backends/llvm/codegen.h
@@ -0,0 +1,14 @@
+//
+// Curly
+// codegen.h: Header file for codegen.c.
+//
+// Created by jenra.
+// Created on September 7 2020.
+//
+
+#ifndef CODEGEN_H
+#define CODEGEN_H
+
+
+
+#endif /* CODEGEN_H */
